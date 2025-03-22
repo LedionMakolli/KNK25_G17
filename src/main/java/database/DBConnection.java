c6e4cd86306bd275d528d379be959d5271203cf5
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-03-22 19:22:32 +0100

    Testim 2.0

---
+++ src/main/java/database/DBConnection.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static Connection connection = null;
+    private static final String DBURL = "jdbc:postgresql://192.168.31.61:5432/knk_ushtrimet";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "123";
+
+    public static Connection getConnection() {
+        if (connection == null){
+            try{
+                 connection = DriverManager.getConnection(DBURL,USER,PASSWORD);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
---
+++ src/main/java/testim/test.java
@@ -0,0 +1,34 @@
+package testim;
+
+import database.DBConnection;
+
+import java.sql.*;
+
+public class test {
+    public static void main(String[] args) {
+
+        try{
+            Connection conn = DBConnection.getConnection();
+            if (conn.isValid(1000)){
+                System.out.println("DB connected");
+            }
+            Statement stm = conn.createStatement();
+
+            stm.execute("INSERT INTO users(name, email, age) VALUES ('Filan Fisteku', 'FFF@gmail.com', 27);");
+
+            ResultSet resultSet = stm.executeQuery("SELECT * FROM users");
+
+            while (resultSet.next()){
+                int id = resultSet.getInt("id");
+                String emri = resultSet.getString("name");
+                String email = resultSet.getString ("email");
+                int age = resultSet.getInt("age");
+
+                System.out.println("ID: " + id + "Emri: " + emri + "Email: " + email + "Age: " + age);
+            }
+            conn.close();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+}
