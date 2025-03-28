849521bd6530e86c8de9229263a459a1f43c840b
duazogu <161737785+duazogu@users.noreply.github.com>
2025-03-28 22:50:21 +0100

    Update Rezervimet.java
    konstruktori privat per klasen Rezervimet + disa permiresime ne deklarimin e anetareve te klases

--- src/main/java/models/Rezervimet.java
+++ src/main/java/models/Rezervimet.java
@@ -4,7 +4,18 @@ public class Rezervimet {
     private int ID_Rezervimet;
     private int ID_Klienti;
     private int ID_Vetura;
-    private String Data_Filimit;
+    private String Data_Fillimit;
     private String Data_Mbarimit;
+    private String Statusi;
     // statusi me enum e zene apo e lire
+
+private Rezervimet(int ID_Rezervimet, int ID_Klienti, int ID_Vetura, String Data_Fillimit, String Data_Mbarimit, String Statusi){
+    this.ID_Rezervimet=ID_Rezervimet;
+    this.ID_Klienti=ID_Klienti;
+    this.ID_Vetura=ID_Vetura;
+    this.Data_Fillimit=Data_Fillimit;
+    this.Data_Mbarimit=Data_Mbarimit;
+    this.Statusi=Statusi;
+}
+
 }
