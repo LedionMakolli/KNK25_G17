0e303346587f8fc4c21b47561e382a2c09486daa
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-03-28 20:33:19 +0100

    Update KlineteRepository
    Update

--- src/main/java/repository/KlientetRepository.java
+++ src/main/java/repository/KlientetRepository.java
@@ -2,7 +2,11 @@ package repository;
 
 import database.DBConnection;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import models.Klientet;
 
 
 public class KlientetRepository {
@@ -12,5 +16,18 @@ public class KlientetRepository {
         this.connection= DBConnection.getConnection();
     }
     // definimi i 5 metodave: getAll, getById, create, update, delete
-
+    public ArrayList<Klientet> getAll() {
+        ArrayList<Klientet> klientet=new ArrayList<>();
+        String query= "SELECT * FROM KLIENTET";
+        try {
+            Statement statement=this.connection.createStatement();
+            ResultSet resultSet=statement.executeQuery(query);
+            while(resultSet.next()) {
+                klientet.add(Klientet.getInstance(resultSet));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return klientet;
+    }
 }
