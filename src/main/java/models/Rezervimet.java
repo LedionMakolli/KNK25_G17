023a2566e5e14d90c6372820de445c55ce5096da
duazogu <161737785+duazogu@users.noreply.github.com>
2025-03-28 23:15:25 +0100

    Update Rezervimet.java
    shtimi i metodes getInstance() qe te kemi te mundur inicializimin e objektit Rezervimet nga te dhenat ne ResultSet

--- src/main/java/models/Rezervimet.java
+++ src/main/java/models/Rezervimet.java
@@ -1,21 +1,34 @@
 package models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Rezervimet {
-    private int ID_Rezervimet;
-    private int ID_Klienti;
-    private int ID_Vetura;
-    private String Data_Fillimit;
-    private String Data_Mbarimit;
-    private String Statusi;
+    private int id_rezervimet;
+    private int id_klienti;
+    private int id_vetura;
+    private String data_fillimit;
+    private String data_mbarimit;
+    private String statusi;
     // statusi me enum e zene apo e lire
 
 private Rezervimet(int ID_Rezervimet, int ID_Klienti, int ID_Vetura, String Data_Fillimit, String Data_Mbarimit, String Statusi){
-    this.ID_Rezervimet=ID_Rezervimet;
-    this.ID_Klienti=ID_Klienti;
-    this.ID_Vetura=ID_Vetura;
-    this.Data_Fillimit=Data_Fillimit;
-    this.Data_Mbarimit=Data_Mbarimit;
-    this.Statusi=Statusi;
+    this.id_rezervimet=ID_Rezervimet;
+    this.id_klienti=ID_Klienti;
+    this.id_vetura=ID_Vetura;
+    this.data_fillimit=Data_Fillimit;
+    this.data_mbarimit=Data_Mbarimit;
+    this.statusi=Statusi;
+}
+public static Rezervimet getInstance(ResultSet resultSet) throws SQLException {
+int id_rezervimet=resultSet.getInt("id_rezervimet");
+int id_klienti=resultSet.getInt("id_klienti");
+int id_vetura=resultSet.getInt("id_vetura");
+String data_fillimit=resultSet.getString("data_fillimit");
+String data_mbarimit =resultSet.getString("data_mbarimit");
+String statusi=resultSet.getString("statusi");
+
+return new Rezervimet(id_rezervimet, id_klienti, id_vetura, data_fillimit, data_mbarimit, statusi);
 }
 
 }
