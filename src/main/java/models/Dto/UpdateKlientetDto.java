1a542cd6bb02a62105bd39792c487edab48a150b
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-03-28 19:27:06 +0100

    Krijimi i UpdateKlientetDto

--- src/main/java/models/Dto/UpdateKlientetDto.java
+++ src/main/java/models/Dto/UpdateKlientetDto.java
@@ -3,6 +3,35 @@ package models.Dto;
 public class UpdateKlientetDto {
     private String Emri;
     private String Mbiemri;
-    private int Nr_personal;
     private String Nr_telefonit;
+
+    public UpdateKlientetDto(String emri, String mbiemri, String Nr_telefonit) {
+        this.Emri=emri;
+        this.Mbiemri=mbiemri;
+        this.Nr_telefonit=Nr_telefonit;
+    }
+
+    public void setEmri(String emri) {
+        Emri = emri;
+    }
+
+    public void setMbiemri(String mbiemri) {
+        Mbiemri = mbiemri;
+    }
+
+    public void setNr_telefonit(String nr_telefonit) {
+        Nr_telefonit = nr_telefonit;
+    }
+
+    public String getEmri() {
+        return Emri;
+    }
+
+    public String getMbiemri() {
+        return Mbiemri;
+    }
+
+    public String getNr_telefonit() {
+        return Nr_telefonit;
+    }
 }
