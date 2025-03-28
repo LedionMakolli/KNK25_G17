01d06fd68fd51c2e72c98a4135dbdfe8e71e7b48
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-03-28 20:35:24 +0100

    Update KlientetRepository.java

--- src/main/java/repository/KlientetRepository.java
+++ src/main/java/repository/KlientetRepository.java
@@ -6,29 +6,27 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-<<<<<<< Updated upstream
 import models.Klientet;
 
-=======
 import models.Dto.CreateKlientetDto;
 import models.Dto.UpdateKlientetDto;
 import models.Klientet;
->>>>>>> Stashed changes
 
 public class KlientetRepository {
     private Connection connection;
 
     public KlientetRepository() {
-        this.connection= DBConnection.getConnection();
+        this.connection = DBConnection.getConnection();
     }
+
     // definimi i 5 metodave: getAll, getById, create, update, delete
     public ArrayList<Klientet> getAll() {
-        ArrayList<Klientet> klientet=new ArrayList<>();
-        String query= "SELECT * FROM KLIENTET";
+        ArrayList<Klientet> klientet = new ArrayList<>();
+        String query = "SELECT * FROM KLIENTET";
         try {
-            Statement statement=this.connection.createStatement();
-            ResultSet resultSet=statement.executeQuery(query);
-            while(resultSet.next()) {
+            Statement statement = this.connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
                 klientet.add(Klientet.getInstance(resultSet));
             }
         } catch (SQLException e) {
@@ -36,8 +34,4 @@ public class KlientetRepository {
         }
         return klientet;
     }
-<<<<<<< Updated upstream
 }
-=======
-}
->>>>>>> Stashed changes
