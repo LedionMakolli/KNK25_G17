5344d0f91d84dd789fa4c9cff5f7f8a9c3b815ef
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-03-29 23:16:32 +0100

    Update test_repositoryt.java

--- src/main/java/testim/test_repositoryt.java
+++ src/main/java/testim/test_repositoryt.java
@@ -23,15 +23,17 @@ public class test_repositoryt {
 ////         Test per funksionimin e tabeles Veturat
 
         VeturatRepository veturatRepository=new VeturatRepository();
-////      CreateVeturatDto vetura1=new CreateVeturatDto("01-977-PE", "Ferrari", "BLACK",
-////                2015,new BigDecimal(120000), 5, Karburanti.BENZINE, 200, Statusi_Vetura.NE_DISPOZICION);
-////        veturatRepository.create(vetura1);
+//      CreateVeturatDto vetura1=new CreateVeturatDto("01-977-PE", "Ferrari", "BLACK",
+//                2015,new BigDecimal(120000), 5, Karburanti.BENZINE, 200, Statusi_Vetura.NE_DISPOZICION);
+//        veturatRepository.create(vetura1);
 //        Veturat vetura2=veturatRepository.getById(4);
 //        ArrayList<Veturat> veturat=veturatRepository.getAll();
 //
 //        veturat.forEach(
 //                veturat1 -> {
-//                    veturat1.printoTeDhenatPerVeturen();
+//                    if(veturat1.getStatusi()==Statusi_Vetura.NE_DISPOZICION) {
+//                        veturat1.printoTeDhenatPerVeturen();
+//                    }
 //                }
 //        );
 
