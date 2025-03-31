012cb06f4b09121e650f302bdb72eb9413909fa7
duazogu <161737785+duazogu@users.noreply.github.com>
2025-03-31 14:53:39 +0200

    Enum + Rezervimet
    Rregullimi i statusti me enum dhe permiresimi i klases Rezervimet

--- src/main/java/models/Rezervimet.java
+++ src/main/java/models/Rezervimet.java
@@ -1,5 +1,7 @@
 package models;
 
+import models.enums.Statusi_Rezervimet;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -9,16 +11,16 @@ public class Rezervimet {
     private int id_vetura;
     private String data_fillimit;
     private String data_mbarimit;
-    private String statusi;
+    private Statusi_Rezervimet statusi_rezervimet;
     // statusi me enum e zene apo e lire
 
-private Rezervimet(int id_rezervimet, int id_klienti, int id_vetura, String data_fillimit, String data_mbarimit, String statusi){
+private Rezervimet(int id_rezervimet, int id_klienti, int id_vetura, String data_fillimit, String data_mbarimit, Statusi_Rezervimet statusi_rezervimet){
     this.id_rezervimet=id_rezervimet;
     this.id_klienti=id_klienti;
     this.id_vetura=id_vetura;
     this.data_fillimit=data_fillimit;
     this.data_mbarimit=data_mbarimit;
-    this.statusi=statusi;
+    this.statusi_rezervimet=statusi_rezervimet;
 }
 public static Rezervimet getInstance(ResultSet resultSet) throws SQLException {
 int id_rezervimet=resultSet.getInt("id_rezervimet");
@@ -26,9 +28,13 @@ int id_klienti=resultSet.getInt("id_klienti");
 int id_vetura=resultSet.getInt("id_vetura");
 String data_fillimit=resultSet.getString("data_fillimit");
 String data_mbarimit =resultSet.getString("data_mbarimit");
-String statusi=resultSet.getString("statusi");
+String statusiString=resultSet.getString("statusi_rezervimet");
 
-return new Rezervimet(id_rezervimet, id_klienti, id_vetura, data_fillimit, data_mbarimit, statusi);
+Statusi_Rezervimet statusi_rezervimet= Statusi_Rezervimet.valueOf(statusiString);
+
+
+
+return new Rezervimet(id_rezervimet, id_klienti, id_vetura, data_fillimit, data_mbarimit, statusi_rezervimet);
 }
 public int getId_rezervimet(){return id_rezervimet;}
 
@@ -40,6 +46,6 @@ public String getData_fillimit(){return data_fillimit;}
 
 public String getData_mbarimit(){return data_mbarimit;}
 
-public String getStatusi(){return statusi;}
+public Statusi_Rezervimet getStatusi(){return statusi_rezervimet;}
 
 }
