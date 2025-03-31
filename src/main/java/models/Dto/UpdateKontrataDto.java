d3ac085186d249ce6a5895a753060d88ac515f0b
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-03-31 14:07:39 +0200

    Update UpdateKontrataDto
    Krijimi i konstruktorit si dhe krijimi i geters

--- src/main/java/models/Dto/UpdateKontrataDto.java
+++ src/main/java/models/Dto/UpdateKontrataDto.java
@@ -1,7 +1,33 @@
 package models.Dto;
 
+import models.enums.Pagesa;
+
 public class UpdateKontrataDto {
-    private int ID_Kontrata;
-    private int ID_Rezervimet;
+    private int id_kontrata;
+    private int id_rezervimet;
     private double shuma;
+    private Pagesa pagesa;
+
+    public UpdateKontrataDto(int id_kontrata, int id_rezervimet, double shuma, Pagesa pagesa) {
+        this.id_kontrata = id_kontrata;
+        this.id_rezervimet = id_rezervimet;
+        this.shuma = shuma;
+        this.pagesa = pagesa;
+    }
+
+    public int getId_kontrata() {
+        return id_kontrata;
+    }
+
+    public int getId_rezervimet() {
+        return id_rezervimet;
+    }
+
+    public double getShuma() {
+        return shuma;
+    }
+
+    public Pagesa getPagesa() {
+        return pagesa;
+    }
 }
