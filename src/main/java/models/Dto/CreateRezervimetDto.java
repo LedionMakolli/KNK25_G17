d364821f61338a6949a71ae12b0b6923a38e0c73
duazogu <161737785+duazogu@users.noreply.github.com>
2025-03-31 17:14:03 +0200

    Create ne RezervimetRepository

--- src/main/java/models/Dto/CreateRezervimetDto.java
+++ src/main/java/models/Dto/CreateRezervimetDto.java
@@ -1,19 +1,24 @@
 package models.Dto;
 
+import models.enums.Statusi_Rezervimet;
+
+import java.sql.Date;
+
 public class CreateRezervimetDto {
     private int id_klienti;
     private int id_vetura;
-    private String data_fillimit;
-    private String data_mbarimit;
-    private String statusi;
+    private Date data_fillimit;
+    private Date data_mbarimit;
+    private Statusi_Rezervimet statusi;
 
-    public CreateRezervimetDto(int id_klienti, int id_vetura, String data_fillimit, String data_mbarimit, String statusi){
+    public CreateRezervimetDto(int id_klienti, int id_vetura, Date data_fillimit, Date data_mbarimit, Statusi_Rezervimet statusi){
         this.id_klienti=id_klienti;
         this.id_vetura=id_vetura;
         this.data_fillimit=data_fillimit;
         this.data_mbarimit=data_mbarimit;
         this.statusi=statusi;
     }
+
     public int getId_klienti(){
         return id_klienti;
     }
@@ -30,27 +35,27 @@ public class CreateRezervimetDto {
       this.id_vetura=id_vetura;
     }
 
-    public String getData_fillimit(){
+    public Date getData_fillimit(){
         return data_fillimit;
     }
 
-    public void setData_fillimit(String data_fillimit){
+    public void setData_fillimit(Date data_fillimit){
         this.data_fillimit=data_fillimit;
     }
 
-    public String getData_mbarimit(){
+    public Date getData_mbarimit(){
         return data_mbarimit;
     }
 
-    public void setData_mbarimit(String data_mbarimit){
+    public void setData_mbarimit(Date data_mbarimit){
         this.data_mbarimit=data_mbarimit;
     }
 
-    public String getStatusi(){
+    public Statusi_Rezervimet getStatusi(){
         return statusi;
     }
 
-    public void setStatusi(String statusi){
+    public void setStatusi(Statusi_Rezervimet statusi){
         this.statusi=statusi;
     }
 
--- src/main/java/repository/RezervimetRepository.java
+++ src/main/java/repository/RezervimetRepository.java
@@ -1,6 +1,8 @@
 package repository;
 
 import database.DBConnection;
+import models.Dto.CreateKlientetDto;
+import models.Dto.CreateRezervimetDto;
 import models.Rezervimet;
 
 import java.sql.*;
@@ -16,7 +18,7 @@ public RezervimetRepository() throws SQLException{
     }
 }
 
-//1. Metoda getALL()
+//1. Metoda getALL
 public ArrayList<Rezervimet> getAll() {
     ArrayList<Rezervimet> rezervimet = new ArrayList<>();
     String query = "SELCET * FROM Rezervimet";
@@ -31,7 +33,7 @@ public ArrayList<Rezervimet> getAll() {
     }
     return rezervimet;
 }
-//2. Metoda getById()
+//2. Metoda getById
     public Rezervimet getById(int id_rezervimet){
     String query ="SELECT * FROM Rezervimet where id_rezervimet=?";
     try{
@@ -46,4 +48,29 @@ public ArrayList<Rezervimet> getAll() {
     }
     return null;
 }
+//3. Metoda create
+
+    public Rezervimet create(CreateRezervimetDto rezervimetDto){
+    String query = """
+            INSERT INTO Rezervimet (id_klienti, id_vetura, data_fillimit, data_mbarimit, statusi_rezervimet)
+            VALUES (?,?,?,?,?)""";
+    try{
+        PreparedStatement pstm = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        pstm.setInt(1, rezervimetDto.getId_klienti());
+        pstm.setInt(2, rezervimetDto.getId_vetura());
+        pstm.setDate(3, rezervimetDto.getData_fillimit());
+        pstm.setDate(4, rezervimetDto.getData_mbarimit());
+        pstm.setObject(5, rezervimetDto.getStatusi(), Types.OTHER);
+        pstm.execute();
+
+        ResultSet rs = pstm.getGeneratedKeys();
+        if(rs.next()){
+            int id = rs.getInt(1);
+            return this.getById(id);
+        }
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+    return null;
+ }
 }
