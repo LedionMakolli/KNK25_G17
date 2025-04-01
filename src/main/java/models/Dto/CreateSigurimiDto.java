5e433663132df31d0a669c98ad825f42b0ad4fd7
Flon15 <kastratiflon@gmail.com>
2025-04-02 00:30:20 +0200

    updatesigurimi done

--- src/main/java/models/Dto/CreateSigurimiDto.java
+++ src/main/java/models/Dto/CreateSigurimiDto.java
@@ -1,4 +1,55 @@
 package models.Dto;
 
+import java.util.Date;
+
 public class CreateSigurimiDto {
+    private String kompania;
+    private Date data_fillimit;
+    private Date data_mbarimit;
+    private double kosto;
+
+
+    private CreateSigurimiDto(int id_sigurimi, int id_vetura, String kompania, Date data_fillimit, Date data_mbarimit, double kosto) {
+
+        this.kompania = kompania;
+        this.data_fillimit = data_fillimit;
+        this.data_mbarimit = data_mbarimit;
+        this.kosto = kosto;
+    }
+
+
+    public String getKompania() {
+        return kompania;
+    }
+
+    public Date getDataFillimit() {
+        return data_fillimit;
+    }
+
+    public Date getDataMbarimit() {
+        return data_mbarimit;
+    }
+
+    public double getKosto() {
+        return kosto;
+    }
+
+
+public void setKompania(String kompania){
+    this.kompania=kompania;
 }
+    public void setData_fillimit(Date data_fillimit){
+        this.data_fillimit=data_fillimit;
+    }
+
+    public void setData_mbarimit(Date data_mbarimit){
+        this.data_mbarimit=data_mbarimit;
+    }
+
+    public void setKosto(double kosto){
+        this.kosto=kosto;
+    }
+}
+
+
+
--- src/main/java/models/Dto/UpdateSigurimiDto.java
+++ src/main/java/models/Dto/UpdateSigurimiDto.java
@@ -1,4 +1,45 @@
 package models.Dto;
 
+import java.util.Date;
+
 public class UpdateSigurimiDto {
+
+Date data_fillimit;
+Date data_mbarimit;
+double kosto;
+
+
+    private UpdateSigurimiDto(  Date data_fillimit, Date data_mbarimit, double kosto) {
+
+
+        this.data_fillimit = data_fillimit;
+        this.data_mbarimit = data_mbarimit;
+        this.kosto = kosto;
+    }
+
+
+    public Date getDataFillimit() {
+        return data_fillimit;
+    }
+
+    public Date getDataMbarimit() {
+        return data_mbarimit;
+    }
+
+    public double getKosto() {
+        return kosto;
+    }
+
+
+    public void setData_fillimit(Date data_fillimit){
+        this.data_fillimit=data_fillimit;
+    }
+
+    public void setData_mbarimit(Date data_mbarimit){
+        this.data_mbarimit=data_mbarimit;
+    }
+
+    public void setKosto(double kosto){
+        this.kosto=kosto;
+    }
 }
