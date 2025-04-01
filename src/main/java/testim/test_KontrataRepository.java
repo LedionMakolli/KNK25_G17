7db7c266038aa16604ed98748d01db79ed0ad71f
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-01 23:57:52 +0200

    Krijimi i Klases tes_KontrataReposiroty
    kitjimi dhe testimi i metodave te klases KontrataRepository

---
+++ src/main/java/testim/test_KontrataRepository.java
@@ -0,0 +1,34 @@
+package testim;
+import models.Dto.CreateKontrataDto;
+import models.Dto.UpdateKontrataDto;
+import models.Kontrata;
+import models.enums.Pagesa;
+import repository.KontrataRepository;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+
+public class test_KontrataRepository {
+    public static void main(String[] args) {
+
+
+        try {
+            Date date = new Date(System.currentTimeMillis());
+            
+            KontrataRepository kontrataRepository = new KontrataRepository();
+            Kontrata kontrata = kontrataRepository.getById(4);
+            System.out.println("Kontrata ID: " + kontrata.getIdkontrata());
+
+            CreateKontrataDto createKontrataDto = new CreateKontrataDto(6,3,5000.0,Pagesa.KARTELE,date);
+            Kontrata newKontrata = kontrataRepository.create(createKontrataDto);
+            newKontrata.printoTeDhenatPerKontraten();
+
+//        UpdateKontrataDto updateKontrataDto = new UpdateKontrataDto(4,2000.0, Pagesa.KESH,date);
+//        kontrataRepository.update(updateKontrataDto);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+    }
+}
