292b0e152b439b855c8b5ec6a9ff88e5ba33edd8
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-01 15:20:49 +0200

    perfundimi i CreateVleresimetDto.java

--- src/main/java/models/Dto/CreateVlersimetDto.java
+++ src/main/java/models/Dto/CreateVlersimetDto.java
@@ -1,9 +1,47 @@
 package models.Dto;
 
 public class CreateVlersimetDto {
-    private int id_vlersimi;
     private int id_klienti;
     private int id_vetura;
     private String text;
     private String data;
+
+    public CreateVlersimetDto(int id_klienti, int id_vetura, String text, String data) {
+        this.id_klienti = id_klienti;
+        this.id_vetura = id_vetura;
+        this.text = text;
+        this.data = data;
+    }
+
+    public void setId_klienti(int id_klienti) {
+        this.id_klienti = id_klienti;
+    }
+
+    public void setId_vetura(int id_vetura) {
+        this.id_vetura = id_vetura;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getId_klienti() {
+        return id_klienti;
+    }
+
+    public int getId_vetura() {
+        return id_vetura;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
