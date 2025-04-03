e6a4f4b800293590d295a93e018e6f0014f5cb0d
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-03 22:33:55 +0200

    rename i variablave me camelCase te Vleresimet

--- src/main/java/models/Vleresimet.java
+++ src/main/java/models/Vleresimet.java
@@ -4,43 +4,43 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Vleresimet {
-    private int id_vleresimi;
-    private int id_klienti;
-    private int id_vetura;
+    private int idVleresimi;
+    private int idKlienti;
+    private int idVetura;
     private int rating;
     private String text;
     private String data;
 
-    private Vleresimet(int id_vleresimi, int id_klienti, int id_vetura, int rating, String text, String data) {
-        this.id_vleresimi = id_vleresimi;
-        this.id_klienti = id_klienti;
-        this.id_vetura = id_vetura;
+    private Vleresimet(int idVleresimi, int idKlienti, int idVetura, int rating, String text, String data) {
+        this.idVleresimi = idVleresimi;
+        this.idKlienti = idKlienti;
+        this.idVetura = idVetura;
         this.rating = rating;
         this.text = text;
         this.data = data;
     }
 
     public static Vleresimet getInstance(ResultSet rs) throws SQLException {
-        int id_vleresimi = rs.getInt("id_vleresimi");
-        int id_klienti = rs.getInt("id_klienti");
-        int id_vetura = rs.getInt("id_vetura");
+        int idVleresimi = rs.getInt("idVleresimi");
+        int idKlienti = rs.getInt("idKlienti");
+        int idVetura = rs.getInt("idVetura");
         int rating = rs.getInt("rating");
         String text = rs.getString("text");
         String data = rs.getString("data");
 
-        return new Vleresimet(id_vleresimi, id_klienti, id_vetura, rating, text, data);
+        return new Vleresimet(idVleresimi, idKlienti, idVetura, rating, text, data);
     }
 
-    public int getId_vleresimi() {
-        return id_vleresimi;
+    public int getIdVleresimi() {
+        return idVleresimi;
     }
 
-    public int getId_klienti() {
-        return id_klienti;
+    public int getIdKlienti() {
+        return idKlienti;
     }
 
-    public int getId_vetura() {
-        return id_vetura;
+    public int getIdVetura() {
+        return idVetura;
     }
 
     public int getRating() {
