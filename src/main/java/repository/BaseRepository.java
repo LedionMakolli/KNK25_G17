3be6eca63d997c7f9b4a20530da71a1ce0c97c4b
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-04 16:53:21 +0200

    inicializimi i struktures se BaseRepository
    - krijimi i klases abstrakte BaseRepository
    - implementimi i metodes getById

--- src/main/java/repository/BaseRepository.java
+++ src/main/java/repository/BaseRepository.java
@@ -1,6 +1,38 @@
 package repository;
 
-public class BaseRepository {
-    // klasa kryesore per klasat e tjera me trashigu prej saj
-    // variablat me i ba cammelCase
+import database.DBConnection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+abstract class BaseRepository<Model, CreateModelDto, UpdateModelDto> {
+    protected Connection connection;
+    private String tableName;
+
+    public BaseRepository(String tableName) {
+        this.connection = DBConnection.getConnection();
+        this.tableName = tableName;
+    }
+
+    abstract Model fromResultSet(ResultSet rs);
+
+    // metoda getById
+    Model getById (int id) {
+        String query = "SELECT * FROM " + this.tableName + " WHERE id = ?";
+        try{
+            PreparedStatement statement = this.connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery(query);
+            if(rs.next()) {
+                return this.fromResultSet(rs);
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+
 }
+
