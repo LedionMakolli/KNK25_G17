4eaa174a655d7dbd40490bb2d0f89b794dceee81
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-04-05 18:52:51 +0200

    Perfundimi i klases Stafi duke perdorur trashegimine nga klasa User

--- src/main/java/models/Stafi.java
+++ src/main/java/models/Stafi.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class Stafi extends User {
@@ -7,10 +9,48 @@ public class Stafi extends User {
     private String pozita;  // "Menaxher", "Staf", "Mekanik"
     private LocalDate dataPunesimit;
 
-    private Stafi(int id, String emri, String mbiemri, String email, String password, String roli, int idStafi, String pozitaS, LocalDate dataPunesimit) {
-        super(id, emri, mbiemri, email, password, roli);
+    private Stafi(int id, String emri, String mbiemri, String email, String password, String nrTelefonit, String roli, int idStafi, String pozita, LocalDate dataPunesimit) {
+        super(id, emri, mbiemri, email, password, nrTelefonit, roli);
         this.idStafi = idStafi;
         this.pozita = pozita;
         this.dataPunesimit = dataPunesimit;
+    }
+    public static Stafi getInstance(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String emri = resultSet.getString("emri");
+        String mbiemri = resultSet.getString("mbiemri");
+        String email=resultSet.getString("email");
+        String password=resultSet.getString("password");
+        String nrTelefonit = resultSet.getString("nrtelefonit");
+        String roli=resultSet.getString("roli");
+        int idStafi=resultSet.getInt("idstafi");
+        String pozita = resultSet.getString("pozita");
+        LocalDate datapunesimit=resultSet.getDate("datapunesimit").toLocalDate();
+        return new Stafi(id,emri,mbiemri,email,password,nrTelefonit,roli,idStafi,pozita,datapunesimit);
+    }
+
+    public int getIdStafi() {
+        return idStafi;
+    }
+
+    public String getPozita() {
+        return pozita;
+    }
+
+    public LocalDate getDataPunesimit() {
+        return dataPunesimit;
+    }
+    public void printoTeDhenatPerStafin() {
+        System.out.println("Punetori i stafit u gjet:");
+        System.out.println("ID: " + getId());
+        System.out.println("Emri: " + getEmri());
+        System.out.println("Mbiemri: " + getMbiemri());
+        System.out.println("Email: " + getEmail()); // passwordi
+        System.out.println("Telefoni: " + getNrTelefonit());
+        System.out.println("Roli: " + getRoli());
+        System.out.println("Id Stafi: " + getIdStafi());
+        System.out.println("Pozita: " + getPozita());
+        System.out.println("Data e punesimit: " + getDataPunesimit());
+        System.out.println("------------------------------");
     }
 }
