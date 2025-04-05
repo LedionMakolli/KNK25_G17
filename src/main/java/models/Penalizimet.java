1c0e8ac21e565347aac92c8c8b74dd27659da79e
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-05 16:21:09 +0200

    krijimi i variablave te klasa Penalizimet

--- src/main/java/models/Penalizimet.java
+++ src/main/java/models/Penalizimet.java
@@ -1,5 +1,13 @@
 package models;
 
-public class Penalizimet
-{
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Penalizimet {
+    private int id;
+    private int idRezervimet;
+    private String arsyeja;
+    private BigDecimal shuma;
+    private LocalDateTime data;
+    private boolean paguar;
 }
