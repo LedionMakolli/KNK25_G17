bbcdfda710ec5c9c1fb2654122712742db5b0966
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-05 23:41:45 +0200

    ndryshimi i vetive me cammelCase

--- src/main/java/models/Rezervimet.java
+++ src/main/java/models/Rezervimet.java
@@ -7,54 +7,54 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Rezervimet {
-    private int id_rezervimet;
-    private int id_klienti;
-    private int id_vetura;
-    private Date data_fillimit;   //LocalDate
-    private Date data_mbarimit;
-    private StatusiRezervimetEnum statusi_rezervimet; //mundemi me shtu vetine created_at per kohen e sakte kur eshte shtuar rreshti tek rezervimet
+    private int id;
+    private int idKlienti;
+    private int idVetura;
+    private Date dataFillimit;   //LocalDate
+    private Date dataMbarimit;
+    private StatusiRezervimetEnum statusiRezervimet; //mundemi me shtu vetine created_at per kohen e sakte kur eshte shtuar rreshti tek rezervimet
 
 
-private Rezervimet(int id_rezervimet, int id_klienti, int id_vetura, Date data_fillimit, Date data_mbarimit, StatusiRezervimetEnum statusi_rezervimet){
-    this.id_rezervimet=id_rezervimet;
-    this.id_klienti=id_klienti;
-    this.id_vetura=id_vetura;
-    this.data_fillimit=data_fillimit;
-    this.data_mbarimit=data_mbarimit;
-    this.statusi_rezervimet=statusi_rezervimet;
+private Rezervimet(int id, int idKlienti, int idVetura, Date dataFillimit, Date dataMbarimit, StatusiRezervimetEnum statusiRezervimet){
+    this.id=id;
+    this.idKlienti=idKlienti;
+    this.idVetura=idVetura;
+    this.dataFillimit=dataFillimit;
+    this.dataMbarimit=dataMbarimit;
+    this.statusiRezervimet=statusiRezervimet;
 }
 public static Rezervimet getInstance(ResultSet resultSet) throws SQLException {
-int id_rezervimet=resultSet.getInt("id_rezervimet");
-int id_klienti=resultSet.getInt("id_klienti");
-int id_vetura=resultSet.getInt("id_vetura");
-Date data_fillimit=resultSet.getDate("data_fillimit");
-Date data_mbarimit =resultSet.getDate("data_mbarimit");
-String statusiString=resultSet.getString("statusi_rezervimet");
+int id=resultSet.getInt("id");
+int idKlienti=resultSet.getInt("idKlienti");
+int idVetura=resultSet.getInt("idVetura");
+Date dataFillimit=resultSet.getDate("dataFillimit");
+Date dataMbarimit =resultSet.getDate("dataMbarimit");
+String statusiString=resultSet.getString("statusiRezervimet");
 
-StatusiRezervimetEnum statusi_rezervimet= StatusiRezervimetEnum.valueOf(statusiString);
+StatusiRezervimetEnum statusiRezervimet= StatusiRezervimetEnum.valueOf(statusiString);
 
 
 
-return new Rezervimet(id_rezervimet, id_klienti, id_vetura, data_fillimit, data_mbarimit, statusi_rezervimet);
+return new Rezervimet(id, idKlienti, idVetura, dataFillimit, dataMbarimit, statusiRezervimet);
 }
-public int getId_rezervimet(){return id_rezervimet;}
+public int getIdRezervimet(){return id;}
 
-public int getId_klienti(){return id_klienti;}
+public int getIdKlienti(){return idKlienti;}
 
-public int getId_vetura(){return id_vetura;}
+public int getIdVetura(){return idVetura;}
 
-public Date getData_fillimit(){return data_fillimit;}
+public Date getDataFillimit(){return dataFillimit;}
 
-public Date getData_mbarimit(){return data_mbarimit;}
+public Date getDataMbarimit(){return dataMbarimit;}
 
-public StatusiRezervimetEnum getStatusi(){return statusi_rezervimet;}
+public StatusiRezervimetEnum getStatusi(){return statusiRezervimet;}
 
     public void printoTeDhenatRezervimet(){
-    System.out.println("ID_Rezervimet: " + getId_rezervimet());
-    System.out.println("ID_Klienti: " + getId_klienti());
-    System.out.println("ID_Vetura: " + getId_vetura());
-    System.out.println("Data_Fillimit: " + getData_fillimit());
-    System.out.println("Data_Mbarimit: " + getData_mbarimit());
+    System.out.println("ID_Rezervimet: " + getIdRezervimet());
+    System.out.println("ID_Klienti: " + getIdKlienti());
+    System.out.println("ID_Vetura: " + getIdVetura());
+    System.out.println("Data_Fillimit: " + getDataFillimit());
+    System.out.println("Data_Mbarimit: " + getDataMbarimit());
     System.out.println("Statusi: " + getStatusi());
     }
 }
