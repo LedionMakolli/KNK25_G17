4c19eb033310f2b77b8e79985c2158616d3aafb1
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-05 22:12:53 +0200

    Perfundimi i Dto-ve per Mirembajtjen

--- src/main/java/models/Dto/CreateMirembajtjaDto.java
+++ src/main/java/models/Dto/CreateMirembajtjaDto.java
@@ -1,4 +1,60 @@
 package models.Dto;
 
+import models.enums.StatusiMirembatjaEnum;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class CreateMirembajtjaDto {
+    private int idMirembajtja;
+    private int idVetura;
+    private String pershkrimi;
+    private Date dataFillimit;
+    private Date dataMbarimit;
+    private BigDecimal kosto;
+    private StatusiMirembatjaEnum statusi;
+    private Integer idStafi;
+
+    public CreateMirembajtjaDto(int idMirembajtja, int idVetura, Date dataFillimit, String pershkrimi, Date dataMbarimit, BigDecimal kosto, StatusiMirembatjaEnum statusi, Integer idStafi) {
+        this.idMirembajtja = idMirembajtja;
+        this.idVetura = idVetura;
+        this.dataFillimit = dataFillimit;
+        this.pershkrimi = pershkrimi;
+        this.dataMbarimit = dataMbarimit;
+        this.kosto = kosto;
+        this.statusi = statusi;
+        this.idStafi = idStafi;
+    }
+
+    public int getIdMirembajtja() {
+        return idMirembajtja;
+    }
+
+    public int getIdVetura() {
+        return idVetura;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public Date getDataFillimit() {
+        return dataFillimit;
+    }
+
+    public Date getDataMbarimit() {
+        return dataMbarimit;
+    }
+
+    public BigDecimal getKosto() {
+        return kosto;
+    }
+
+    public StatusiMirembatjaEnum getStatusi() {
+        return statusi;
+    }
+
+    public Integer getIdStafi() {
+        return idStafi;
+    }
 }
--- src/main/java/models/Dto/UpdateMirembajtjaDto.java
+++ src/main/java/models/Dto/UpdateMirembajtjaDto.java
@@ -1,4 +1,60 @@
 package models.Dto;
 
+import models.enums.StatusiMirembatjaEnum;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class UpdateMirembajtjaDto {
+    private int idMirembajtja;
+    private int idVetura;
+    private String pershkrimi;
+    private Date dataFillimit;
+    private Date dataMbarimit;
+    private BigDecimal kosto;
+    private StatusiMirembatjaEnum statusi;
+    private Integer idStafi;
+
+
+    public UpdateMirembajtjaDto(int idMirembajtja, String pershkrimi, Date dataFillimit, Date dataMbarimit, BigDecimal kosto, StatusiMirembatjaEnum statusi, Integer idStafi) {
+        this.idMirembajtja = idMirembajtja;
+        this.pershkrimi = pershkrimi;
+        this.dataFillimit = dataFillimit;
+        this.dataMbarimit = dataMbarimit;
+        this.kosto = kosto;
+        this.statusi = statusi;
+        this.idStafi = idStafi;
+    }
+
+    public int getIdMirembajtja() {
+        return idMirembajtja;
+    }
+
+    public int getIdVetura() {
+        return idVetura;
+    }
+
+    public String getPershkrimi() {
+        return pershkrimi;
+    }
+
+    public Date getDataFillimit() {
+        return dataFillimit;
+    }
+
+    public Date getDataMbarimit() {
+        return dataMbarimit;
+    }
+
+    public BigDecimal getKosto() {
+        return kosto;
+    }
+
+    public StatusiMirembatjaEnum getStatusi() {
+        return statusi;
+    }
+
+    public Integer getIdStafi() {
+        return idStafi;
+    }
 }
