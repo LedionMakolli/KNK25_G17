94437215cdd5e0159d2fa577f53ca94e0ba14673
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-05 14:39:51 +0200

    rename i variables id te Ofertat & Vleresimet

--- src/main/java/models/Ofertat.java
+++ src/main/java/models/Ofertat.java
@@ -5,14 +5,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Ofertat {
-    private int idOferta;
+    private int id;
     private int idVetura;
     private double zbritja;
     private Date dataFillimit;
     private Date dataMbarimit;
 
-    private Ofertat(int idOferta, int idVetura, double zbritja, Date dataFillimit, Date dataMbarimit) {
-        this.idOferta = idOferta;
+    private Ofertat(int id, int idVetura, double zbritja, Date dataFillimit, Date dataMbarimit) {
+        this.id = id;
         this.idVetura = idVetura;
         this.zbritja = zbritja;
         this.dataFillimit = dataFillimit;
@@ -20,17 +20,17 @@ public class Ofertat {
     }
 
     public static Ofertat getInstance(ResultSet resultSet) throws SQLException {
-        int idOferta = resultSet.getInt("idOferta");
+        int id = resultSet.getInt("id");
         int idVetura = resultSet.getInt("idVetura");
         double zbritja = resultSet.getDouble("zbritja");
         Date dataFillimit = resultSet.getDate("dataFillimit");
         Date dataMbarimit = resultSet.getDate("dataMbarimit");
 
-        return new Ofertat(idOferta, idVetura, zbritja, dataFillimit, dataMbarimit);
+        return new Ofertat(id, idVetura, zbritja, dataFillimit, dataMbarimit);
     }
 
-    public int getIdOferta() {
-        return idOferta;
+    public int getId() {
+        return id;
     }
 
     public int getIdVetura() {
--- src/main/java/models/Vleresimet.java
+++ src/main/java/models/Vleresimet.java
@@ -5,15 +5,15 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 public class Vleresimet {
-    private int idVleresimi;
+    private int id;
     private int idKlienti;
     private int idVetura;
     private int rating;
     private String text;
     private Timestamp data;
 
-    private Vleresimet(int idVleresimi, int idKlienti, int idVetura, int rating, String text, Timestamp data) {
-        this.idVleresimi = idVleresimi;
+    private Vleresimet(int id, int idKlienti, int idVetura, int rating, String text, Timestamp data) {
+        this.id = id;
         this.idKlienti = idKlienti;
         this.idVetura = idVetura;
         this.rating = rating;
@@ -22,18 +22,18 @@ public class Vleresimet {
     }
 
     public static Vleresimet getInstance(ResultSet rs) throws SQLException {
-        int idVleresimi = rs.getInt("idVleresimi");
+        int id = rs.getInt("id");
         int idKlienti = rs.getInt("idKlienti");
         int idVetura = rs.getInt("idVetura");
         int rating = rs.getInt("rating");
         String text = rs.getString("text");
         Timestamp data = rs.getTimestamp("data");
 
-        return new Vleresimet(idVleresimi, idKlienti, idVetura, rating, text, data);
+        return new Vleresimet(id, idKlienti, idVetura, rating, text, data);
     }
 
-    public int getIdVleresimi() {
-        return idVleresimi;
+    public int getId() {
+        return id;
     }
 
     public int getIdKlienti() {
