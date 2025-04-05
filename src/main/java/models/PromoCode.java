de7b605a7d749065caa5bdb540d566da754052c7
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-05 15:25:46 +0200

    krijimi i variablave te klasa PromoCode

--- src/main/java/models/PromoCode.java
+++ src/main/java/models/PromoCode.java
@@ -1,4 +1,11 @@
 package models;
 
+import java.time.LocalDate;
+
 public class PromoCode {
+    private int id;
+    private String kodi;
+    private double zbritja;
+    private LocalDate dataSkadimit;
+    private boolean aktiv;
 }
