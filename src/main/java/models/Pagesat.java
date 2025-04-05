42a26ecd921dedee3fd5df0cdce56446cd2ca5f5
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-05 15:32:18 +0200

    krijimi i variablave te klasa Pagesat

---
+++ src/main/java/models/Pagesat.java
@@ -0,0 +1,16 @@
+package models;
+
+import models.enums.Pagesa;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Pagesat {
+    private int id;
+    private int idRezervimi;
+    private Pagesa lloji;
+    private Integer promoCodeId;  // Integer n'vend te int per vlera NULL
+    private BigDecimal totaliPaZbritje;
+    private BigDecimal totaliFinal;  // me ose pa zbritje
+    private LocalDateTime data;
+}
