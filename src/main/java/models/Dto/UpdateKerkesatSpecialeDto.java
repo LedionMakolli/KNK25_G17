298b59526682983ad894a9cc604968da20a27e1a
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-06 00:38:46 +0200

    Update UpdateKerkesatSpecialeDto.java
    fillimi i krijimi te klases UpdateKerkesatSpecialeDto

--- src/main/java/models/Dto/UpdateKerkesatSpecialeDto.java
+++ src/main/java/models/Dto/UpdateKerkesatSpecialeDto.java
@@ -1,4 +1,17 @@
 package models.Dto;
 
 public class UpdateKerkesatSpecialeDto {
+    private int id;
+    private int idRezervimet;
+    private String kerkese;
+    private boolean plotesuar;
+
+    public UpdateKerkesatSpecialeDto(int id, int idRezervimet, String kerkese, boolean plotesuar){
+        this.id=id;
+        this.idRezervimet=idRezervimet;
+        this.kerkese=kerkese;
+        this.plotesuar=plotesuar;
+    }
+  public UpdateKerkesatSpecialeDto(){}
+
 }
