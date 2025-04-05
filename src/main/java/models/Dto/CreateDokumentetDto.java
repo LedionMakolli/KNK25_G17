359ccc6dcb1b10207b5565adc3410abe8b018d3b
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-04-05 22:41:16 +0200

    FInalizmi i Dto-ve te klases Dokumentet

--- src/main/java/models/Dto/CreateDokumentetDto.java
+++ src/main/java/models/Dto/CreateDokumentetDto.java
@@ -1,4 +1,39 @@
 package models.Dto;
 
+import java.sql.Date;
+
 public class CreateDokumentetDto {
+    private int idDokument;
+    private int idKontrata;
+    private String lloji;
+    private String path;
+    private Date dataUpload;
+
+    public CreateDokumentetDto(int idDokument, int idKontrata, String lloji, String path, Date dataUpload) {
+        this.idDokument = idDokument;
+        this.idKontrata = idKontrata;
+        this.lloji = lloji;
+        this.path = path;
+        this.dataUpload = dataUpload;
+    }
+
+    public int getIdDokument() {
+        return idDokument;
+    }
+
+    public int getIdKontrata() {
+        return idKontrata;
+    }
+
+    public String getLloji() {
+        return lloji;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Date getDataUpload() {
+        return dataUpload;
+    }
 }
--- src/main/java/models/Dto/UpdateDokumentetDto.java
+++ src/main/java/models/Dto/UpdateDokumentetDto.java
@@ -1,4 +1,41 @@
 package models.Dto;
 
+import models.enums.StatusiMirembatjaEnum;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class UpdateDokumentetDto {
+    private int idDokument;
+    private int idKontrata;
+    private String lloji;
+    private String path;
+    private Date dataUpload;
+
+    public UpdateDokumentetDto(int idDokument, String lloji, String path, Date dataUpload) {
+        this.idDokument = idDokument;
+        this.lloji = lloji;
+        this.path = path;
+        this.dataUpload = dataUpload;
+    }
+
+    public int getIdDokument() {
+        return idDokument;
+    }
+
+    public int getIdKontrata() {
+        return idKontrata;
+    }
+
+    public String getLloji() {
+        return lloji;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Date getDataUpload() {
+        return dataUpload;
+    }
 }
