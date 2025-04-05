6f750dfe19984f0962c26b73153551dcfae785f4
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-06 00:45:14 +0200

    Update UpdateKerkesatSpecialeDto.java
    krijimi i klases UpdateKerkesatSpecialeDto

--- src/main/java/models/Dto/UpdateKerkesatSpecialeDto.java
+++ src/main/java/models/Dto/UpdateKerkesatSpecialeDto.java
@@ -14,4 +14,26 @@ public class UpdateKerkesatSpecialeDto {
     }
   public UpdateKerkesatSpecialeDto(){}
 
+    public int getId(){
+        return id;
+    }
+public int getIdRezervimet(){
+        return idRezervimet;
+}
+public void setIdRezervimet(int idRezervimet){
+        this.idRezervimet=idRezervimet;
+}
+public String getKerkese(){
+    return kerkese;
+}
+public void setKerkese(String kerkese){
+        this.kerkese=kerkese;
+}
+public boolean isPlotesuar(){
+        return plotesuar;
+}
+public void setPlotesuar(boolean plotesuar){
+        this.plotesuar=plotesuar;
+}
+
 }
