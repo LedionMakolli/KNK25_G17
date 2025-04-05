95d28291354920a9cad14a6431402fca41483d7a
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-05 14:59:22 +0200

    krijimi i variablave te klasa User

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,10 @@
 package models;
 
 public class User {
+    private int id;
+    private String emri;
+    private String mbiemri;
+    private String email;
+    private String password;
+    private String roli; // "ADMIN", "CLIENT", "STAFF"
 }
