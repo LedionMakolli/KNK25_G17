1d3e57c7f31b42fdb42cd884b4e7b3c57162bd80
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-06 22:53:18 +0200

    Krijimi i klases PromoCode

--- src/main/java/models/PromoCode.java
+++ src/main/java/models/PromoCode.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public class PromoCode {
@@ -7,5 +9,45 @@ public class PromoCode {
     private String kodi;
     private double zbritja;
     private LocalDate dataSkadimit;
-    private boolean aktiv;
+    private Boolean aktiv;
+
+    private PromoCode(int id,String kodi, double zbritja, LocalDate dataSkadimit, Boolean aktiv){
+        this.id=id;
+        this.kodi=kodi;
+        this.zbritja=zbritja;
+        this.dataSkadimit=dataSkadimit;
+        this.aktiv=aktiv;
+    }
+    public static PromoCode getInstance(ResultSet resultSet) throws SQLException{
+        int id=resultSet.getInt("id");
+        String kodi=resultSet.getString("kodi");
+        double zbritja = resultSet.getDouble("zbritja");
+        LocalDate dataSkadimit= resultSet.getDate("dataSkadimit").toLocalDate();
+        Boolean aktiv = resultSet.getBoolean("aktiv");
+
+        return new PromoCode(id, kodi, zbritja, dataSkadimit, aktiv);
+    }
+    public int getId(){
+        return id;
+    }
+    public String getKodi(){
+        return kodi;
+    }
+    public double getZbritja(){
+        return zbritja;
+    }
+    public LocalDate getDataSkadimit(){
+        return dataSkadimit;
+    }
+    public Boolean getAktiv(){
+        return aktiv;
+    }
+    public void printPromoCode(){
+        System.out.println("Te dhenat per PromoCode");
+        System.out.println("ID: " + getId());
+        System.out.println("Kodi: " + getKodi());
+        System.out.println("Zbritja:" + getZbritja());
+        System.out.println("Data e skadimit: " + getDataSkadimit());
+        System.out.println("Aktiv: " + getAktiv());
+    }
 }
