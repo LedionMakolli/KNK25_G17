50f3530ca2e22f8f31aade049bb3de61c08f2d01
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-06 23:55:46 +0200

    Metoda update ne klasen PromoCodeRepository

--- src/main/java/repository/PromoCodeRepository.java
+++ src/main/java/repository/PromoCodeRepository.java
@@ -1,23 +1,24 @@
 package repository;
 
-import models.Dto.CreateKerkesatSpecialeDto;
-import models.Dto.CreatePromoCodeDto;
-import models.Dto.UpdatePromoCodeDto;
-import models.Dto.UpdateRezervimetDto;
+import models.Dto.*;
+import models.KerkesaSpeciale;
 import models.PromoCode;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PromoCodeRepository extends BaseRepository<PromoCode, CreatePromoCodeDto, UpdatePromoCodeDto>{
     public PromoCodeRepository() throws SQLException{
         super("PromoCode");
     }
+
     @Override
-    public PromoCode fromResulSet(ResultSet resultSet){
+    public PromoCode fromResultSet(ResultSet resultSet){
         try{
             return PromoCode.getInstance(resultSet);
         }catch(SQLException e){
@@ -35,7 +36,7 @@ public class PromoCodeRepository extends BaseRepository<PromoCode, CreatePromoCo
             pstm.setString(1, promoCodeDto.getKodi());
             pstm.setDouble(2, promoCodeDto.getZbritja());
             pstm.setDate(3, java.sql.Date.valueOf(promoCodeDto.getDataSkadimit()));
-            pstm.setBoolean(4, promoCodeDto.setAktiv());
+            pstm.setBoolean(4, promoCodeDto.isAktiv());
             pstm.execute();
             ResultSet rs = pstm.getGeneratedKeys();
             if(rs.next()){
@@ -45,5 +46,38 @@ public class PromoCodeRepository extends BaseRepository<PromoCode, CreatePromoCo
         }catch(SQLException e){
             e.printStackTrace();
         } return null;
+    }
+
+
+    public PromoCode update(UpdatePromoCodeDto promoCodeDto) {
+        StringBuilder query = new StringBuilder("UPDATE PROMOCODE SET");
+        List<Object> parameters = new ArrayList<>();
+        boolean hasUpdates = false;
+
+        if (promoCodeDto.getDataSkadimit() != null) {
+            query.append("dataSkadimit=?");
+            parameters.add(promoCodeDto.getDataSkadimit());
+            hasUpdates = true;
+        }
+        if (promoCodeDto.isAktiv() != null) {
+            query.append("aktiv=?");
+            parameters.add(promoCodeDto.isAktiv());
+            hasUpdates = true;
+        }
+
+        query.setLength(query.length() - 2);
+        query.append("where id = ?");
+        parameters.add(promoCodeDto.getId());
+
+        try {
+            PreparedStatement pstm = connection.prepareStatement(query.toString());
+            for (int i = 0; i < parameters.size(); i++) {
+                pstm.setObject(i + 1, parameters.get(i));
+            }
+            pstm.executeUpdate();
+            return getById(promoCodeDto.getId());
+        } catch (SQLException e) {
+            throw new RuntimeException("Gabim gjate perditesimit", e);
+        }
     }
 }
