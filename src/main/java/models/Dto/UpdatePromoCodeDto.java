a89dced84dff4cba353ed1ca03aafebd2b6a909e
duazogu <161737785+duazogu@users.noreply.github.com>
2025-04-06 23:20:54 +0200

    Fillimi i klases UpdatePromoCodeDto

--- src/main/java/models/Dto/UpdatePromoCodeDto.java
+++ src/main/java/models/Dto/UpdatePromoCodeDto.java
@@ -1,4 +1,17 @@
 package models.Dto;
 
+import java.time.LocalDate;
+
 public class UpdatePromoCodeDto {
+    private int id;
+    private LocalDate dataSkadimit;
+    private Boolean aktiv;
+
+    public UpdatePromoCodeDto(int id, LocalDate dataSkadimit, Boolean aktiv){
+        this.id=id;
+        this.dataSkadimit=dataSkadimit;
+        this.aktiv=aktiv;
+    }
+    
+
 }
