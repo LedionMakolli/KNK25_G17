5fb1f874bf9c34b21744a1f38f225ee14e00a1c0
Flon15 <kastratiflon@gmail.com>
2025-04-11 00:45:08 +0200

    error detected

--- src/main/java/models/Dto/CreateLogActivityDto.java
+++ src/main/java/models/Dto/CreateLogActivityDto.java
@@ -1,4 +1,58 @@
 package models.Dto;
 
+
+import models.enums.VeprimetEnum;
+
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 public class CreateLogActivityDto {
+    private int id;
+    private int idUser;
+    private VeprimetEnum veprimi;
+    private LocalDateTime data;
+    private String ipAddress;
+
+    // Constructor
+    public CreateLogActivityDto(int id, int idUser, VeprimetEnum veprimi, LocalDateTime data, String ipAddress) {
+        this.id = id;
+        this.idUser = idUser;
+        this.veprimi = veprimi;
+        this.data = data;
+        this.ipAddress = ipAddress;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public VeprimetEnum getVeprimi() {
+        return veprimi;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    // Setters
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public void setVeprimi(VeprimetEnum veprimi) {
+        this.veprimi = veprimi;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
 }
