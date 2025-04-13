426a39348d3863c1c48871c300c19fea53245f1b
EjoneAnalumi <ejoneanalumi@gmail.com>
2025-04-13 13:37:45 +0200

    ndryshimi i variablave ne gjuhen angleze - UpdateOffersDto

---
+++ src/main/java/models/Dto/UpdateOffersDto.java
@@ -0,0 +1,55 @@
+package models.Dto;
+
+import java.util.Date;
+
+public class UpdateOffersDto {
+    private int id;
+    private Integer carId;
+    private Double discount;
+    private Date startDate;
+    private Date endDate;
+
+    public UpdateOffersDto(int id, int carId, double discount, Date startDate, Date endDate) {
+        this.id = id;
+        this.carId = carId;
+        this.discount = discount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public Double getDiscount() {
+        return discount;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+}
