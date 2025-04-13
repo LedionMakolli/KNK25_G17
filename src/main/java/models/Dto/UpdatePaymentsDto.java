f2b87ba80771f1d7e87b1007d6d7a72763e9590c
Flon15 <kastratiflon@gmail.com>
2025-04-14 01:44:10 +0200

    updated everything to english on the payments

---
+++ src/main/java/models/Dto/UpdatePaymentsDto.java
@@ -0,0 +1,83 @@
+package models.Dto;
+
+import models.enums.PagesaEnum;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class UpdatePaymentsDto {
+    private int id;
+    private Integer idReservation;
+    private PagesaEnum type;
+    private LocalDateTime date;
+    private Integer promoCodeId;
+    private BigDecimal totalNoDiscount;
+    private BigDecimal totalFinal;
+
+    // Constructor
+    public UpdatePaymentsDto(int id, int idReservation, PagesaEnum type, Integer promoCodeId,
+                             BigDecimal totalNoDiscount, BigDecimal totalFinal, LocalDateTime date) {
+        this.id = id;
+        this.idReservation = idReservation;
+        this.type = type;
+        this.promoCodeId = promoCodeId;
+        this.totalNoDiscount = totalNoDiscount;
+        this.totalFinal = totalFinal;
+        this.date = date;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public Integer getIdReservation() {
+        return idReservation;
+    }
+
+    public PagesaEnum getType() {
+        return type;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public Integer getPromoCodeId() {
+        return promoCodeId;
+    }
+
+    public BigDecimal getTotalNoDiscount() {
+        return totalNoDiscount;
+    }
+
+    public BigDecimal getTotalFinal() {
+        return totalFinal;
+    }
+
+    // Setters
+
+
+
+
+    public void setType(PagesaEnum type) {
+        this.type = type;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public void setPromoCodeId(Integer promoCodeId) {
+        this.promoCodeId = promoCodeId;
+    }
+
+    public void setTotalNoDiscount(BigDecimal totalNoDiscount) {
+        this.totalNoDiscount = totalNoDiscount;
+    }
+
+    public void setTotalFinal(BigDecimal totalFinal) {
+        this.totalFinal = totalFinal;
+    }
+}
+
---
+++ src/main/java/repository/PaymentsRepository.java
@@ -0,0 +1,102 @@
+package repository;
+
+import models.Dto.CreatePaymentsDto;
+import models.Dto.UpdatePaymentsDto;
+import models.Payments;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaymentsRepository extends BaseRepository<Payments, CreatePaymentsDto, UpdatePaymentsDto> {
+    public PaymentsRepository() throws SQLException {
+        super("pagesat");
+    }
+    @Override
+    public Payments fromResultSet(ResultSet rs) {
+        try {
+            return Payments.getInstance(rs);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    // 3. metoda create
+    public Payments create(CreatePaymentsDto paymentsDto) {
+        String query= """
+                INSERT INTO PAGESAT (ID, IDRESERVATION, TYPE, PROMOCODEID,
+                TOTALNODISCOUNT, TOTALFINAL, DATE)
+                VALUES (?, ?, ?, ?, ?, ?, ?,)
+                """;
+        try {
+            PreparedStatement preparedStatement= connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, paymentsDto.getId());
+            preparedStatement.setInt(2, paymentsDto.getIdReservation());
+            preparedStatement.setObject(3, paymentsDto.getType(),Types.OTHER);
+            preparedStatement.setInt(4, paymentsDto.getPromoCodeId());
+            preparedStatement.setBigDecimal(5, paymentsDto.getTotalNoDiscount());
+            preparedStatement.setBigDecimal(6, paymentsDto.getTotalFinal());
+            preparedStatement.setTimestamp(7, java.sql.Timestamp.valueOf(paymentsDto.getDate()));
+
+            preparedStatement.execute();
+            ResultSet result=preparedStatement.getGeneratedKeys();
+            if(result.next()) {
+                int id=result.getInt(1);
+                return this.getById(id);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    // 4. metoda update
+    public Payments update(UpdatePaymentsDto PaymentsDto) {
+        StringBuilder query = new StringBuilder("UPDATE PAYMENTS SET ");
+        List<Object> parametrat = new ArrayList<>();
+        boolean hasUpdates=false;
+
+        if (PaymentsDto.getType() != null) {
+            query.append("type = ?, ");
+            parametrat.add(PaymentsDto.getType());
+            hasUpdates=true;
+        }
+        if (PaymentsDto.getPromoCodeId() != null) {
+            query.append("promocodeid = ?, ");
+            parametrat.add(PaymentsDto.getPromoCodeId());
+            hasUpdates=true;
+        }
+        if (PaymentsDto.getTotalNoDiscount() !=null) {
+            query.append("totalnodiscount = ?, ");
+            parametrat.add(PaymentsDto.getTotalNoDiscount());
+            hasUpdates=true;
+        }
+        if (PaymentsDto.getTotalFinal() != null) {
+            query.append("totalfinal = ?, ");
+            parametrat.add(PaymentsDto.getTotalFinal());
+            hasUpdates=true;
+        }
+        if (PaymentsDto.getDate() != null) {
+            query.append("date = ?, ");
+            parametrat.add(PaymentsDto.getDate());
+            hasUpdates=true;
+        }
+        if (!hasUpdates) {
+            return getById(PaymentsDto.getId());
+        }
+        query.setLength(query.length()-2);
+        query.append(" WHERE id=?");
+        parametrat.add(PaymentsDto.getId());
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(query.toString());
+            for(int i=0; i<parametrat.size(); i++) {
+                preparedStatement.setObject(i+1, parametrat.get(i), Types.OTHER);
+            }
+            preparedStatement.executeUpdate();
+            return getById(PaymentsDto.getId());
+        } catch (SQLException e) {
+            throw new RuntimeException("Error during update!", e);
+        }
+    }
+
+}
