4d2268ee24b5021248742b562596abb352fc1e8f
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-05-06 14:29:59 +0200

    Shtimi i LanguageManager, SceneManager, SceneLocator

---
+++ src/main/java/services/SceneManager.java
@@ -0,0 +1,78 @@
+package services;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import utils.SceneLocator;
+
+public class SceneManager {
+    private static SceneManager sceneManager;
+    private Scene scene;
+    private LanguageManager languageManager;
+    private String currentPath;
+
+    private SceneManager(){
+        this.languageManager = LanguageManager.getInstance();
+        this.currentPath = SceneLocator.HOME_PAGE;
+        this.scene = this.initScene();
+    }
+
+    public static SceneManager getInstance(){
+        if(sceneManager == null)
+            sceneManager = new SceneManager();
+        return sceneManager;
+    }
+
+    private Scene initScene(){
+        try{
+            return new Scene(this.getParent(currentPath));
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+
+    public static void load(String path) throws Exception{
+        if(sceneManager == null){
+            throw new Exception("Scene manager is not initialized yet!");
+        }
+        sceneManager.loadParent(path);
+    }
+
+    public static void load(String path, Pane pane) throws Exception{
+        if(sceneManager == null){
+            throw new Exception("Scene manager is not initialized yet!");
+        }
+        sceneManager.loadParent(path, pane);
+    }
+
+    private void loadParent(String path) throws Exception{
+        Parent parent = getParent(path);
+        this.currentPath = path;
+        scene.setRoot(parent);
+    }
+
+    private void loadParent(String path, Pane pane) throws Exception{
+        pane.getChildren().clear();
+
+        Parent parent = getParent(path);
+        pane.getChildren().add(parent);
+    }
+
+    private Parent getParent(String path) throws Exception{
+        FXMLLoader loader = new FXMLLoader(
+                this.getClass().getResource(path)
+        );
+        loader.setResources(this.languageManager.getResourceBundle());
+        return loader.load();
+    }
+
+    public static void reload() throws Exception{
+        load(sceneManager.currentPath);
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+}
---
+++ src/main/java/utils/SceneLocator.java
@@ -0,0 +1,7 @@
+package utils;
+
+public class SceneLocator {
+    public static final String HOME_PAGE = "/group_03/views/home.fxml";
+    public static final String CREATE_USER_PAGE = "/group_03/views/create_user.fxml";
+//    ...
+}
