0d0e0aa4c5aa0515884ca40dbf457b7f17de5035
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-05-06 15:37:13 +0200

    Permiresime ne main.java, shtimi i locale dhe resourcebundle

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,11 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(
-                Main.class.getResource("/views/create_client.fxml")
+                Main.class.getResource("create_client.fxml")
         );
+        Locale locale=new Locale("sq", "Kosova", "");
+        ResourceBundle resources=ResourceBundle.getBundle("languages.messages", locale);
+        fxmlLoader.setResources(resources);
         Scene scene = new Scene(fxmlLoader.load(), 700, 500);
         stage.setTitle("Create Client");
         stage.setScene(scene);
