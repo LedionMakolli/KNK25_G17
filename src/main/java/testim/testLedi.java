7d2418ab0d558ac88ffee244da731d3020014e5c
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-05-07 00:05:47 +0200

    Testime per LoginService

--- src/main/java/testim/testLedi.java
+++ src/main/java/testim/testLedi.java
@@ -3,7 +3,11 @@ import models.Contract;
 import models.Dto.*;
 
 import models.enums.PaymentEnum;
+import repository.ClientRepository;
 import repository.ContractRepository;
+import repository.LogActivityRepository;
+import repository.StaffRepository;
+import services.LoginService;
 
 import java.sql.Date;
 import java.sql.SQLException;
@@ -14,45 +18,16 @@ public class testLedi {
         try {
             Date date = new Date(System.currentTimeMillis());
 
-            // KONTRATA TEST
-            
-            ContractRepository contractRepository = new ContractRepository();
-            Contract contract = contractRepository.getById(1);
-            System.out.println("Contract ID: " + contract.getId());
+            ClientRepository clientRepository = new ClientRepository();
+            StaffRepository staffRepository = new StaffRepository();
+            LogActivityRepository logActivityRepository = new LogActivityRepository();
+            LoginService loginService = new LoginService(clientRepository,staffRepository,logActivityRepository);
 
-            CreateContractDto createContractDto = new CreateContractDto(6,1,1,5000.0,date);
-            Contract newContract = contractRepository.create(createContractDto);
-            newContract.printoTeDhenatPerKontraten();
+            LoginResponse response = loginService.login("ledi123","123");
 
-            UpdateContractDto updateContractDto = new UpdateContractDto(4,2000.0,date);
-            contractRepository.update(updateContractDto);
-
-            // DOKUMENTET TEST
-
-//            DocumentsRepository dokumentetRepository = new DocumentsRepository();
-//            Documents dokumentet = dokumentetRepository.getById(2);
-//            System.out.println("Documents ID: " + dokumentet.getId());
-//
-//            CreateDokumentetDto createDokumentetDto = new CreateDokumentetDto(4, "PDF", "test.pdf", date);
-//            Documents newDokumentet = dokumentetRepository.create(createDokumentetDto);
-//            newDokumentet.printoTeDhenatPerDokumentin();
-////
-//            UpdateDocumentsDto updateDokumentetDto = new UpdateDocumentsDto(3,"IMG","test",date);
-//            newDokumentet = dokumentetRepository.update(updateDokumentetDto);
-//            newDokumentet.printoTeDhenatPerDokumentin();
-
-
-//            MaintenanceRepository mirembajtjaRepository = new MaintenanceRepository();
-//            Maintenance mirembajtja = mirembajtjaRepository.getById(1);
-//            System.out.println("Maintenance ID: " + mirembajtja.getId());
-//
-//            CreateMaintenanceDto createMirembajtjaDto = new CreateMaintenanceDto(1, date, "test", date, new BigDecimal("45.50"), StatusMaintenanceEnum.IN_PROCESS, 1);
-//            Maintenance newMireMbajtja = mirembajtjaRepository.create(createMirembajtjaDto);
-//            newMireMbajtja.printoTeDhenatPerMirembajtjen();
-//
-//            UpdateMaintenanceDto updateMirembajtjaDto = new UpdateMaintenanceDto(1,"MIRE",date,date,new BigDecimal("45.00"),StatusMaintenanceEnum.CANCELED);
-//            newMireMbajtja = mirembajtjaRepository.update(updateMirembajtjaDto);
-//            newMireMbajtja.printoTeDhenatPerMirembajtjen();
+            System.out.println("Login successful!");
+            System.out.println("User type: " + response.getUsername());
+            System.out.println("Username: " + response.getRole());
 
         }catch (SQLException e){
             e.printStackTrace();
