ae36ceefc04962b21b43b758837d00836ca1b95b
Muhamedjakupi1 <muhamedjakupi056@gmail.com>
2025-05-07 12:10:51 +0200

    Perfundimi i ruajtjes se passwordit si salted Hash

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,26 +1,26 @@
 package app;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import services.LanguageManager;
-import services.SceneManager;
-import java.util.Locale;
+import services.PasswordHasher;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        SceneManager manager = SceneManager.getInstance();
-        LanguageManager languageManager=LanguageManager.getInstance();
-
-        languageManager.setLocale(new Locale("en"));
-
-        stage.setScene(manager.getScene());
-        stage.setTitle("Log In");
-        stage.show();
-    }
-    
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        // Krijo një salt dhe password të salted
+        String salt = PasswordHasher.generateSalt();
+        String saltedPassword = PasswordHasher.generateSaltedHash("medimedi", salt);
+
+        // Testo verifikimin
+        boolean isValid = PasswordHasher.compareSaltedHash("medimedi", salt, saltedPassword);
+
+        System.out.println("Salti: " + salt);
+        System.out.println("Passwordi i kriposur: " + saltedPassword);
+        // Printo rezultatin
+        if (isValid) {
+            System.out.println("Fjalekalimi eshte i sakte.");
+        } else {
+            System.out.println("Fjalekalimi eshte gabim.");
+        }
+
+        // (Opsional: Mund të shtosh kodin për JavaFX, nëse duhen interfejsa)
+        // launch(args); // Kjo mund të lëshohet vetëm nëse po krijoni GUI me JavaFX
     }
 }
