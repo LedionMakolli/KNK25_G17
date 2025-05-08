0ef740327de09ab7d45778cab6062251cb4a229d
duazogu <161737785+duazogu@users.noreply.github.com>
2025-05-08 22:50:45 +0200

    Vazhdimi i krijimit te klasave ChangePasswordController dhe ChangePasswordService
    deklarimi i variablave private changePasswordService, currentUsername, currentRole, konstruktrin e controller, setUserInfo, fillimi i metodes handleSaveNewPassword(butoni), showAlert metoda(te gjitha keto ne kontroller)

--- src/main/java/controllers/ChangePasswordController.java
+++ src/main/java/controllers/ChangePasswordController.java
@@ -1,7 +1,9 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
+import services.ChangePasswordService;
 
 public class ChangePasswordController {
     @FXML
@@ -13,5 +15,50 @@ public class ChangePasswordController {
     @FXML
     private PasswordField pwdFieldConfirm;
 
+    private ChangePasswordService changePasswordService;
+
+    private String currentUsername;
+    private String currentRole;
+
+    public ChangePasswordController(){
+        try{
+            this.changePasswordService = new ChangePasswordService();
+        }catch(Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+    public void setUserInfo(String username, String role ){
+        this.currentUsername = username;
+        this.currentRole = role;
+    }
+
+    @FXML
+    private void handleSaveNewPassword(){
+        String oldPassword = pwdFieldOld.getText().trim();
+        String newPassword = pwdFieldNew.getText().trim();
+        String confirmPassword = pwdFieldConfirm.getText().trim();
+
+        if (oldPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+            showAlert(Alert.AlertType.WARNING, "Kujdes", "Plotësoni të gjitha fushat.");
+            return;
+        }
+        if(!newPassword.equals(confirmPassword)){
+            showAlert(Alert.AlertType.ERROR, "Gabim", "Fjalekalimi i ri dhe konfirmimi nuk perputhen");
+            return;
+        }
+
+        
+    }
+
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
     
 }
--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -3,5 +3,6 @@ package utils;
 public class SceneLocator {
     public static final String SIGNUP_PAGE = "/views/create_client.fxml";
     public static final String LOGIN_PAGE="/views/login.fxml";
+    public static final String CHANGE_PASSWORD_PAGE = "/views/changepassword.fxml";
 //    ...
 }
