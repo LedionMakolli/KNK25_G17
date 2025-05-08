2cef941a9f7b30d305f45495f33605d20112676a
duazogu <161737785+duazogu@users.noreply.github.com>
2025-05-08 21:00:52 +0200

    krijimi i ChangePassword

---
+++ src/main/java/controllers/ChangePasswordController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+
+public class ChangePasswordController {
+    @FXML
+    private PasswordField pwdFieldOld;
+
+    @FXML
+    private PasswordField pwdFieldNew;
+
+    @FXML
+    private PasswordField pwdFieldConfirm;
+
+    
+}
