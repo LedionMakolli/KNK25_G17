efe27593764a14c38e95419187f70bacc2e136a0
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-05-11 00:53:32 +0200

    Funksionalizmi i getById per te paraqitur kontraten perkatese per klient

--- src/main/java/controllers/ControllerPerWelcome.java
+++ src/main/java/controllers/ControllerPerWelcome.java
@@ -2,10 +2,15 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import services.SceneManager;
 import utils.SceneLocator;
 //TA DINI QE QIKJO VEQ PROVE PER BUTON
 public class ControllerPerWelcome {
+    @FXML
+    private Button btnContracts;
+
+
     @FXML
     private void ClickOnChangePassword() {
         try {
@@ -17,11 +22,29 @@ public class ControllerPerWelcome {
         }
     }
 
+    @FXML
+    public void seeContracts(){
+        try{
+            SceneManager.load(SceneLocator.SEE_CONTRACTS);
+        }catch (Exception e){
+            showErrorAlert("Unable to load Contracts",
+                    "An error occurred while trying to load the contracts page.");
+        }
+    }
+
     private void showAlert(Alert.AlertType alertType, String title, String message) {
         Alert alert = new Alert(alertType);
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void showErrorAlert(String header, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(header);
+        alert.setContentText(content);
         alert.showAndWait();
     }
 }
