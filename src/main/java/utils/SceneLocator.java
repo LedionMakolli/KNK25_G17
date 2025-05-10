789d386af8a190ab5531ed657cab949bc252f3a9
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-05-11 00:08:05 +0200

    Krijmi i nje scene per Contracts

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -6,5 +6,6 @@ public class SceneLocator {
     public static final String LOGIN_PAGE="/views/login.fxml";
     public static final String CHANGE_PASSWORD_PAGE = "/views/changepassword.fxml";
     public static final String SEE_PENALTIES="/views/penalties.fxml";
+    public static final String SEE_CONTRACTS="/views/contracts.fxml";
 //    ...
 }
