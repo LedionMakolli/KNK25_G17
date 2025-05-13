eea011bc2c5d1b6f0f38d1494768c7eacb9ded18
duazogu <161737785+duazogu@users.noreply.github.com>
2025-05-14 01:32:54 +0200

    shtimi i view ne Scene Locator

--- src/main/java/utils/SceneLocator.java
+++ src/main/java/utils/SceneLocator.java
@@ -10,5 +10,6 @@ public class SceneLocator {
     public static final String SEE_CONTRACTS="/views/contracts.fxml";
     public static final String SEE_CAR_DETAILS= "/views/car_details.fxml";
     public static final String RESERVATION_FORM= "/views/reservation_form.fxml";
+    public static final String CONTRACT_FORM = "/views/contractform.fxml";
 //    ...
 }
