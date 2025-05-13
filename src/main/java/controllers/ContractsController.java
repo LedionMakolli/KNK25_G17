06d96e9fe05fb00d230c86a97d556e2d745393cc
duazogu <161737785+duazogu@users.noreply.github.com>
2025-05-14 01:36:09 +0200

    Update i ContractsController
    kemi lidhur view te contract form me ContractsController ku kemi deklruar variablat te FXML, pastaj kemi implementuar metodat handleSaveClick per ruajtjen e te dhenave ne databaze permes butonit Save. Gjithashtu kemi implementuar metoden handleCacnelClick per te anuluar kontraten

--- src/main/java/controllers/ContractsController.java
+++ src/main/java/controllers/ContractsController.java
@@ -3,16 +3,21 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import models.Contract;
+import models.Dto.CreateContractDto;
 import repository.ContractRepository;
+import services.SceneManager;
 import services.SessionManager;
+import utils.SceneLocator;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.sql.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class ContractsController extends BaseController {
     @FXML
@@ -33,6 +38,26 @@ public class ContractsController extends BaseController {
     @FXML
     private TableColumn<Contract, LocalDateTime> dateColumn;
 
+    @FXML
+    private TextField txtFieldReservationId;
+
+    @FXML
+    private TextField txtFieldPaymentId;
+
+    @FXML
+    private TextField txtFieldAmount;
+
+    @FXML
+    private DatePicker dpDate;
+
+    @FXML
+    private Button btnSave;
+
+    @FXML
+    private Button btnCancel;
+
+
+
     private ContractRepository contractRepository;
 
     @FXML
@@ -42,6 +67,50 @@ public class ContractsController extends BaseController {
             setupTableColumns();
             loadContracts();
         }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleSaveClick(){
+     String reservationIdTxt = txtFieldReservationId.getText();
+     String paymentIdTxt = txtFieldPaymentId.getText();
+     String amountTxt = txtFieldAmount.getText();
+     LocalDate dateDp = dpDate.getValue();
+
+     if(reservationIdTxt.isEmpty() || paymentIdTxt.isEmpty() || amountTxt.isEmpty() || dateDp == null){
+         showAlert(Alert.AlertType.ERROR, "warning.title", "warning.emptyFields");
+         return;
+     }
+
+     int reservationId = Integer.parseInt(reservationIdTxt);
+     int paymentId = Integer.parseInt(paymentIdTxt);
+     double amount = Double.parseDouble(amountTxt);
+     Date date = Date.valueOf(dateDp);
+
+     try{
+         CreateContractDto contractDto = new CreateContractDto(paymentId, reservationId, amount, date);
+
+         ContractRepository contractRepository = new ContractRepository();
+         Contract contract = contractRepository.create(contractDto);
+
+         if(contract != null){
+             SceneManager.load(SceneLocator.SEE_CONTRACTS);
+             showAlert(Alert.AlertType.INFORMATION, "success", "success");
+         }else {
+             showAlert(Alert.AlertType.ERROR, "fail", "fail");
+         }
+     }catch(Exception e){
+         e.printStackTrace();
+         showAlert(Alert.AlertType.ERROR, "fail", "fail");
+              }
+    }
+
+    @FXML
+    private void handleCancelClick(){
+        try{
+            SceneManager.load(SceneLocator.HOME_PAGE);
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
@@ -73,6 +142,4 @@ public class ContractsController extends BaseController {
             e.printStackTrace();
         }
     }
-
-
 }
