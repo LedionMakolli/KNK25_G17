3b9ad2b766b0934394960ea9505063f673cc1e19
ledion <145694386+LedionMakolli@users.noreply.github.com>
2025-05-15 22:07:45 +0200

    Krijimi i About
    krijimi i view about ku tregohen udhezime per perdorimin e programit

---
+++ src/main/java/controllers/AboutController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import services.ChangePasswordService;
+import services.SceneManager;
+import utils.SceneLocator;
+
+public class AboutController extends BaseController{
+
+    }
+
