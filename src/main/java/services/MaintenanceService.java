a3a574ff199c95a75b9eed6eeaad77e2b0808342
duazogu <161737785+duazogu@users.noreply.github.com>
2025-05-17 12:09:40 +0200

    Update MaintenanceService.java
    metoda getMaintenanceByRole, tip kthyes liste te maintenance dhe na ndihmon per shfaqen e mirembajtjeve ne tabele

--- src/main/java/services/MaintenanceService.java
+++ src/main/java/services/MaintenanceService.java
@@ -1,8 +1,11 @@
 package services;
 
+import models.Maintenance;
+import models.Reservations;
 import repository.MaintenanceRepository;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class MaintenanceService {
 
@@ -15,6 +18,17 @@ public class MaintenanceService {
 
     /////////////////////////////////////
 
+public List<Maintenance> getMaintenanceByRole() throws SQLException{
+        List<Maintenance> maintenances;
+        String role = SessionManager.getInstance().getCurrentRole();
 
+        if("staff".equals(role)){
+            int staffId = SessionManager.getInstance().getCurrentStaff().getId();
+            maintenances = maintenanceRepository.getStaffById(staffId);
+        } else{
+            maintenances = maintenanceRepository.getAll();
+        }
+        return maintenances;
+}
 
 }
